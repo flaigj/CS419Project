1e546e0edfa5bbbac5697438e4958c430ca1c932
Frank Eslami <eslamif@onid.oregonstate.edu>
2016-02-25 10:33:49 -0800

    find_open_slot.py logic updated correctly. Command line logic complete.

--- find_open_slots.py
+++ find_open_slots.py
@@ -49,58 +49,57 @@ def createMeetingMatrix():
 	for i in range(0, numOfParticipants):	
 		meetingMatrix.append('Null')			#initiate matrix to null
 	
-	index2 = 0
-	index3 = 0
-	for index, elem in enumerate(participantData):		#Each participant
+	idxActor = 0
+	idxEvent = 0
+	for idxPartpnt, elePartpntm in enumerate(participantData):		#Each participant
 		timeSlots = list()	#temp storage for each participant's time slot
 		#print '----------------------------'
-		#print elem.getName()
-		numOfParticipantSlots = len(participantData[index].getBusyTimeSlot())
+		#print elePartpntm.getName()
+		numOfParticipantSlots = len(participantData[idxPartpnt].getBusyTimeSlot())
 		#print 'numOfParticipantSlots = ', numOfParticipantSlots
 	
-		while(index2 < numOfWindowSlots):		#Actor's desired meeting slots
-			windowSlot = timeWindowSlots[index2]
-			participantSlot = participantData[index].getBusyTimeSlot()
+		while(idxActor < numOfWindowSlots):		#Actor's desired meeting slots
+			windowSlot = timeWindowSlots[idxActor]
+			participantSlot = participantData[idxPartpnt].getBusyTimeSlot()
 	
 			#Compare participant's time slot to Actor's desired meeting slots
-			#print 'index3 = ', index3
-			#print 'index2 = ', index2
-			if(index3 < numOfParticipantSlots):		#Participant time slot
-				participantPosix = func.timeStrToPosix(participantSlot[index3])
+			#print 'idxEvent = ', idxEvent
+			#print 'idxActor = ', idxActor
+			if(idxEvent < numOfParticipantSlots):		#Participant time slot
+				participantPosix = func.timeStrToPosix(participantSlot[idxEvent])
 				print func.posixToPST(participantPosix)
 				print func.posixToPST(windowSlot)
 	
 				if(participantPosix == windowSlot):
 					print 'equal'
 					timeSlots.append(1)		#participant not available in meeting slot
-					index2 += 1		#next Actor's meeting slot
+					idxActor += 1			#next Actor's meeting slot
+					idxEvent += 1			#next participant's time slot
 					#continue	
 	
 				if(participantPosix < windowSlot):
 					print 'less'
 					timeSlots.append(0)		#participant available in meeting slot
-					index2 += 1		#next Actor's meeting slot
-					index3 += 1		#next participant's time slot
+					idxEvent += 1			#next participant's time slot
 					#continue	
 	
 				if(participantPosix > windowSlot):
 					print 'more'
 					timeSlots.append(0)		#participant available in meeting slot
-					index2 += 1		#next Actor's meeting slot
-					index3 += 1		#next participant's time slot
+					idxActor += 1			#next Actor's meeting slot
 					#continue	
 	
 			#Go to next participant if all participant time slots is checked
-			if(index3 == numOfParticipantSlots or index2 == numOfWindowSlots):
+			if(idxEvent == numOfParticipantSlots or idxActor == numOfWindowSlots):
 				#Set remainding meetingMatrix to 1, indicating no availability
-				for j in range(index2, numOfWindowSlots):
+				for j in range(idxActor, numOfWindowSlots):
 					timeSlots.append(1)		#mark remainder as unvailable
 	
-				index2 = 0	#reset iterator to first Actor's meeting slot
-				index3 = 0	#reset iterator to first time slot for next participant
+				idxActor = 0	#reset iterator to first Actor's meeting slot
+				idxEvent = 0	#reset iterator to first time slot for next participant
 				
 				#Store participant availability in meeting matrix
-				meetingMatrix[index] = timeSlots
+				meetingMatrix[idxPartpnt] = timeSlots
 		
 				#print meetingMatrix
 				break		#got to next participant
